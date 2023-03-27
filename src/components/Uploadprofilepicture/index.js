90f86440e9b132b917fd80157e142fdb79cc27ac
Md Rony <mdronyrsk01724@gmail.com>
2023-03-27 23:47:42 +0600

    disegned crop image

--- src/components/Uploadprofilepicture/index.js
+++ src/components/Uploadprofilepicture/index.js
@@ -1,19 +1,60 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { IoMdImages } from "react-icons/io";
+import ImageCropper from "./ImageCropper";
+import "cropperjs/dist/cropper.css";
 import "./style.css";
 
-const Uploadpicture = () => {
+const Uploadpicture = ({ setOpen }) => {
+  const [image, setImage] = useState();
+  const [cropData, setCropData] = useState("#");
+  const [cropper, setCropper] = useState();
   const chooseFile = useRef(null);
+
+  const handleUpdateProfile = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropper !== "undefined") {
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+    }
+  };
+
   return (
     <>
-      <div className="upload_box" onClick={() => chooseFile.current.click()}>
-        <input type="file" hidden ref={chooseFile} />
-        <div className="upload">
+      <div className="upload_box ">
+        <input
+          type="file"
+          hidden
+          ref={chooseFile}
+          onChange={handleUpdateProfile}
+        />
+        <div className="upload" onClick={() => chooseFile.current.click()}>
           <div className="upload_icon">
             <IoMdImages />
           </div>
-          <p>upload photo</p>
+          <p>Upload photo</p>
         </div>
+        {image && (
+          <ImageCropper
+            image={image}
+            setCropper={setCropper}
+            setImage={setImage}
+            cropData={cropData}
+            getCropData={getCropData}
+          />
+        )}
       </div>
     </>
   );
