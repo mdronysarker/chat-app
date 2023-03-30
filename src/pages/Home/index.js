ee26ce026910be582a480f5e47b36e329a3a4b92
Md Rony <mdronyrsk01724@gmail.com>
2023-03-31 00:46:37 +0600

    designed search bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import "./style.css";
+import Searchbox from "../../components/searchbox";
 const Home = () => {
   return (
     <>
-      <h2>hello home</h2>
+      <Grid container className="home_pages">
+        <Grid item xs={4} className="home_items">
+          <div>
+            <Searchbox />
+          </div>
+        </Grid>
+        <Grid item xs={4} className="home_items">
+          mid
+        </Grid>
+        <Grid item xs={4} className="home_items">
+          right
+        </Grid>
+      </Grid>
     </>
   );
 };
