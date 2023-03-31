92b1224df740ed6cdb2404ddb4378f67db85f126
Md Rony <mdronyrsk01724@gmail.com>
2023-03-31 15:04:53 +0600

    design friends for home page

--- src/components/friends/index.js
+++ src/components/friends/index.js
@@ -4,7 +4,51 @@ import "./style.css";
 const Friend = () => {
   return (
     <>
-      <div className="friends"></div>
+      <div className="friends">
+        <div className="friend_header">
+          <h4>Friends</h4>
+        </div>
+        <div className="friend-item-wrraper">
+          <div className="friend-images"></div>
+          <div className="friend-names">
+            <h5>rony</h5>
+          </div>
+          <div className="friend-list-btn">
+            <button type="button">Unfriend</button>
+            <button type="button">Block</button>
+          </div>
+        </div>
+        <div className="friend-item-wrraper">
+          <div className="friend-images"></div>
+          <div className="friend-names">
+            <h5>rony</h5>
+          </div>
+          <div className="friend-list-btn">
+            <button type="button">Unfriend</button>
+            <button type="button">Block</button>
+          </div>
+        </div>
+        <div className="friend-item-wrraper">
+          <div className="friend-images"></div>
+          <div className="friend-names">
+            <h5>rony</h5>
+          </div>
+          <div className="friend-list-btn">
+            <button type="button">Unfriend</button>
+            <button type="button">Block</button>
+          </div>
+        </div>
+        <div className="friend-item-wrraper">
+          <div className="friend-images"></div>
+          <div className="friend-names">
+            <h5>rony</h5>
+          </div>
+          <div className="friend-list-btn">
+            <button type="button">Unfriend</button>
+            <button type="button">Block</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
